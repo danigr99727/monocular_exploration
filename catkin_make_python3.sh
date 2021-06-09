d43acc0af2a5fc6422724b6559c093d68df78f68
danigr <danigr.99727@gmail.com>
2021-06-09 16:23:20 +0000

    gscam added

--- catkin_make_python3.sh
+++ catkin_make_python3.sh
@@ -1,5 +1,5 @@
 #conda init bash
 #conda activate TDNet
 
-catkin_make -DPYTHON_EXECUTABLE=/root/anaconda3/envs/TDNet/bin/python3.7 -DPYTHON_INCLUDE_DIR=/root/anaconda3/envs/TDNet/include/python3.7m/ -DPYTHON_LIBRARY=/root/anaconda3/envs/TDNet/lib/libpython3.7m.so
+catkin_make -DGSTREAMER_VERSION_1_x=On -DPYTHON_EXECUTABLE=/root/anaconda3/envs/TDNet/bin/python3.7 -DPYTHON_INCLUDE_DIR=/root/anaconda3/envs/TDNet/include/python3.7m/ -DPYTHON_LIBRARY=/root/anaconda3/envs/TDNet/lib/libpython3.7m.so
 
